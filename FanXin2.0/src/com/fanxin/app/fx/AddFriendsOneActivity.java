db67d0d6ef034584ffee571a98fc33fb587cf40a
AOE <84543217@qq.com>
2016-02-28 03:14:43 +0800

    凡信工程

---
+++ FanXin2.0/src/com/fanxin/app/fx/AddFriendsOneActivity.java
@@ -0,0 +1,32 @@
+package com.fanxin.app.fx;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.fanxin.app.R;
+import com.fanxin.app.activity.BaseActivity;
+
+public class AddFriendsOneActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addfriends);
+        TextView tv_search=(TextView) this.findViewById(R.id.tv_search);
+        tv_search.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+             startActivity(new Intent(AddFriendsOneActivity.this,AddFriendsTwoActivity.class));                
+            }
+            
+        });
+    }
+    
+    public void back(View view){
+        finish();
+    }
+}
